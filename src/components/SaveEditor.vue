484ab2428739ec984394648b891205f9dd52a944
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-04-18 21:04:36 +0700

    Fix bug

--- src/components/SaveEditor.vue
+++ src/components/SaveEditor.vue
@@ -168,6 +168,7 @@ const saveFile = () => {
             type="text"
             :id="key"
             :value="value"
+            v-model="settings[key]"
             class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Bonnie Green"
           />
